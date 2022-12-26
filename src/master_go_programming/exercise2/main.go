2ed0e40a0d1ff8814516a594866e81fbf2a8e667
Marcelo Cartagena <120571996+marcecarta@users.noreply.github.com>
2022-12-26 19:11:50 -0300

    formating some code

--- src/master_go_programming/exercise2/main.go
+++ src/master_go_programming/exercise2/main.go
@@ -3,9 +3,11 @@ package main
 import "fmt"
 
 const var1 = 123
+const varStr = "Hi there"
 
 func main() {
 	const var2 = 456
 	fmt.Printf("variable 1 %d", var1)
 	fmt.Printf(" variable 2 %d", var2)
+	fmt.Printf(" He say: %s", varStr)
 }
